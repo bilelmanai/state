96589df97fed05dd6418c15baab44b7290b84154
manai bilel <bilel_kata@live.fr>
2021-05-26 12:19:54 +0200

    lastup

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import "./App.css";
 import Profile from "./Profile";
 const App = () => {
   const [count, setCount] = useState(true);
-  const modif = () => setCount(!count);
   return (
     <div>
       {count === true ? <Profile /> : " "}
       <button onClick={() => setCount(!count)}>
-        {count === true ? "clicked" : "off"}
+        {count === true ? "turn off" : "turn on"}
       </button>
     </div>
   );
